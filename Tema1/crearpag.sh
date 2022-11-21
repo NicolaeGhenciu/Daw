d88e65b98c21f01b88243291cb0bd8644548530e
Ghenciu Nicolae Adrian <114905950+NicolaeGhenciu@users.noreply.github.com>
2022-11-21 20:16:50 +0100

    Add files via upload

---
+++ Tema1/crearpag.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "<!DOCTYPE html><html lang='en'><head><title>paginote</title></head><body><h3>Bienvenido al Blog</h3><p>Esto es un texto de la página</p></body></html>">/var/www/html/$1.html
